e86dc8394c0fa455742d33b96af163a8659ac559
ruthvikvachhani <ruthvikvachhani@gmail.com>
2021-08-11 06:14:30 +0000

    day 3 commit after access modifier

---
+++ src/com/tcs/access/modifier/B.java
@@ -0,0 +1,11 @@
+package com.tcs.access.modifier;
+
+public class B {
+
+	public static void main(String[] args) {
+		A a = new A();
+		System.out.println(a.Age);
+
+	}
+
+}
--- src/com/tcs/final1/keyword/Ticket.java
+++ src/com/tcs/final1/keyword/Ticket.java
@@ -1,5 +1,21 @@
 package com.tcs.final1.keyword;
 
-public class Ticket {
+public class Ticket {// we cannot extend this class from other class.
+	private final String movie = "Sholay";// we have to initialize final variable where we define it.
+	private float price;
+	
+	public final void sell() {// final method cannot be override but can be overload
+		
+	}
+	
+	public final void sell(float price) {
+		
+	}
+}
 
+public IllegalTicket extends Ticket{
+	
+	public final void sell() {
+		
+	}
 }
