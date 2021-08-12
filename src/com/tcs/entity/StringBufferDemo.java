2db0ca7e023528ce2e4934c75098a8d1515b898f
ruthvikvachhani <ruthvikvachhani@gmail.com>
2021-08-12 05:43:09 +0000

    day 4 commit after list

---
+++ src/com/tcs/entity/StringBufferDemo.java
@@ -0,0 +1,10 @@
+package com.tcs.entity;
+
+public class StringBufferDemo {
+	public static void main(String[] args) {
+		String literal = "TCS";
+		String newKeyWord = new String("TCS");
+		StringBuilder strBuilder = new StringBuilder("TCS");
+		StringBuffer strBuffer = new StringBuffer("TCS");
+	}
+}
