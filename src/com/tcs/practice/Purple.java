e67c603fd4768c30ce21539d4f08379948e8b770
ruthvikvachhani <ruthvikvachhani@gmail.com>
2021-08-13 12:10:08 +0530

    committing after change in quiz file

--- src/com/tcs/practice/Purple.java
+++ src/com/tcs/practice/Purple.java
@@ -1,29 +1,26 @@
 package com.tcs.practice;
 
-class A extends Exception{}
-class B extends A{}
-class C extends B{}
-public class Purple {
-	public static void main(String[] args) {
-		int a,b,c,d,f,g,x;
-		a=b=c=d=f=g=0;
-		x=3;
-		try {
-			try {
-				switch(x)
-				{
-				case 1:throw new A();
-				case 2:throw new B();
-				case 3:throw new C();
-				}
-				a++;}
-				catch(B e) {b++;}
-				finally {c++;}
-			}
-			catch(A e) {d++;}
-			catch(Exception e) {f++;}
-			finally {g++;}
-			System.out.println(a+","+b+","+c+","+d+","+f+","+g);
-		}
-	}
-
+class Level1Exception extends Exception {}
+class Level2Exception extends Level1Exception {}
+class Level3Exception extends Level2Exception {}
+class Purple {
+  public static void main(String args[]) {
+    int a,b,c,d,f,g,x;
+    a = b = c = d = f = g = 0;
+    x = 1;
+    try {
+      throw new Level1Exception();
+      try {
+        switch (x) {
+          case 1: throw new Level1Exception();
+          case 2: throw new Level2Exception();
+          case 3: throw new Level3Exception();
+      } a++; }
+      catch (Level2Exception e) {b++;}
+      finally {c++;}
+    }
+    catch (Level1Exception e) { d++;}
+    catch (Exception e) {f++;}
+    finally {g++;}
+    System.out.print(a+","+b+","+c+","+d+","+f+","+g);
+}}
